da854fef62bb45aa64fec231a95d92632f70a2f0
m.usman <m.usman@tekboox.net>
2025-04-07 21:37:51 +0500

    fix refresh token

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -20,4 +20,41 @@ axiosInstance.interceptors.request.use(
   }
 );
 
+axiosInstance.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    console.log("status code is = ", error.status);
+    if (error.status === 401) {
+      const refres_token = localStorage.getItem("refresh_token", null);
+      // call function to update access_token in local storage
+      update_token(refres_token);
+      console.log("refres token is = ", refres_token);
+    }
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+
+function update_token(refres_token) {
+  const payload = {
+    refresh: refres_token,
+  };
+  axios
+    .post("http://127.0.0.1:8000/account/token/refresh/", payload)
+    .then((response) => {
+      localStorage.setItem("access_token", response["data"]["access"]);
+    })
+    .catch((error) => {
+      // console.log("catch of refresh token = ", error);
+    })
+    .finally(() => {
+      // console.log("refresh token finally block = ");
+    });
+}
+
 export default axiosInstance;
